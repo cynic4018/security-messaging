ad03cb839af49aac1844dd0b9a8813b84e1c357d
Cynic4018 <q.katapol@gmail.com>
2021-02-24 19:07:14 +0700

    use bcrypt password hashing and set at least 13 in follow BcryptPasswordEncoder

--- src/main/java/ku/message/config/SecurityConfig.java
+++ src/main/java/ku/message/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder encoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(13);
     }
 
     @Override
